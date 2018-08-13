f341c6536a23774a0d4cd90eaa3e6676ab70cc53
MyselfRoy <proy@nyu.edu>
2018-08-13 12:25:48 -0500

    Update firstscript.sh

--- scripts/shell/firstscript.sh
+++ scripts/shell/firstscript.sh
@@ -4,5 +4,5 @@ sudo service httpd start
 sudo chkconfig httpd on
 sudo echo "Hello world" >> /var/www/html/index.html
 sudo chmod 777 /var/www/html/index.html
-sudo chmod root:root /var/www/html/index.html
+sudo chown root:root /var/www/html/index.html
 sudo ls -l /var/www/html/index.html
