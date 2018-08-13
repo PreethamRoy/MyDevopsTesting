3e5681d6cd4f2502447a2767d54c12034bd2e939
MyselfRoy <proy@nyu.edu>
2018-08-13 12:22:22 -0500

    Update firstscript.sh

--- scripts/shell/firstscript.sh
+++ scripts/shell/firstscript.sh
@@ -2,3 +2,7 @@
 yum install -y httpd
 sudo service httpd start
 sudo chkconfig httpd on
+sudo echo "Hello world" >> /var/www/html/index.html
+sudo chmod 777 /var/www/html/index.html
+sudo chmod root:root /var/www/html/index.html
+ls -l /var/www/html/index.html
